299a6bd600e5adf0550f5a36e8cefa8df621f214
Rafhael <rafhael.pereira@gft.com>
2022-06-24 15:44:38 -0300

    sec 01 - lec 07 mono from future

---
+++ src/main/java/com/studies/sec01/Lec07MonoFromFuture.java
@@ -0,0 +1,23 @@
+package com.studies.sec01;
+
+import com.studies.utils.Util;
+import reactor.core.publisher.Mono;
+
+import java.util.concurrent.CompletableFuture;
+
+public class Lec07MonoFromFuture {
+
+    public static void main(String[] args) {
+
+        Mono.fromFuture(getName())
+                .subscribe(Util.onNext());
+
+        Util.sleepSeconds(1);
+
+    }
+
+    private static CompletableFuture<String> getName(){
+        return CompletableFuture.supplyAsync(() -> Util.faker().name().fullName());
+    }
+
+}
