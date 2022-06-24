2d1c737ca515d1375944097f2d99f686a32c1e4d
Rafhael <rafhael.pereira@gft.com>
2022-06-24 15:51:49 -0300

    sec 01 - lec 08 mono from runnable

---
+++ src/main/java/com/studies/sec01/Lec08MonoFromRunnable.java
@@ -0,0 +1,25 @@
+package com.studies.sec01;
+
+import com.studies.utils.Util;
+import reactor.core.publisher.Mono;
+
+public class Lec08MonoFromRunnable {
+
+    public static void main(String[] args) {
+
+        Mono.fromRunnable(timeConsumingProcess())
+                .subscribe(Util.onNext(),
+                        Util.onError(),
+                        () -> System.out.println("Process is done. Sending emails..."));
+
+
+    }
+
+    private static Runnable timeConsumingProcess(){
+        return () -> {
+            Util.sleepSeconds(3);
+            System.out.println("Operation completed");
+        };
+    }
+
+}
