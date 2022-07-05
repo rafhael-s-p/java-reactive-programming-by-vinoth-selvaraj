9be7b38331eb35b7fe4333bfcce5677eaa12f688
Rafhael <rafhael.pereira@gft.com>
2022-07-05 09:04:25 -0300

    sec 02 - lec10 stock price assignment

---
+++ src/main/java/com/studies/sec02/assignment/StockPricePublisher.java
@@ -0,0 +1,20 @@
+package com.studies.sec02.assignment;
+
+import com.studies.utils.Util;
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class StockPricePublisher {
+
+    public static Flux<Integer> getPrice() {
+        AtomicInteger atomicInteger = new AtomicInteger(100);
+        return Flux.interval(Duration.ofSeconds(1))
+                .map(i -> atomicInteger.getAndAccumulate(
+                        Util.faker().random().nextInt(-5, 5),
+                        Integer::sum
+                ));
+    }
+
+}
